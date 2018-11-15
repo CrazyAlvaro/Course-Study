f32414b5e9c98158556a81c739d5cedd7e7e6493
Yechen Huang <yechenhuang@gmail.com>
2018-11-15 13:17:44 +0800

    Node callbacks and Error Handling

--- FullStackWebDevelopment/Course4-Server-Side/week1/node-example/rectangle.js
+++ FullStackWebDevelopment/Course4-Server-Side/week1/node-example/rectangle.js
@@ -1,2 +1,25 @@
 exports.perimeter  = (x, y) => (2*(x+y))
 exports.area = (x, y) => (x*y)
+
+// Pattern, callback(error, object)
+
+module.exports = (x, y, callback) => {
+  // pass in callback function accept two variables, object(err), or object
+  if (x <= 0 || y <= 0)
+    setTimeout(() =>
+      callback(new Error("Rectangle dimensions should be greater than zero: \
+        l = " + x + ", and b = " + y),
+        null),
+      2000);
+  else {
+    setTimeout(() =>
+      // no error, pass back object
+      callback(null, {
+
+        // closure function access x, y
+        perimeter: () => (2*(x+y)),
+        area: () => (x*y)
+      }),
+      2000);
+  }
+}
