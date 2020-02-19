19821bf358f0fee5375af1538fa12d4ae0f65ac8
Yechen Huang <yechenhuang@gmail.com>
2020-02-19 10:46:38 +0800

    Finish Machine Language Assignment

--- ComputerScience/FromNandtoTetris2/nand2tetris/projects/04/fill/Fill.asm
+++ ComputerScience/FromNandtoTetris2/nand2tetris/projects/04/fill/Fill.asm
@@ -12,3 +12,78 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(INITIALIZATION)
+    @action_bit
+    M=0     // action_bit initialize
+    @set
+    M=0     // Original not set
+
+(LISTENING)
+    @KBD
+    D=M
+    @SET
+    D; JNE  // if keyboard has input, set screen
+    @CLEAR
+    D; JEQ  // clear if keyboard has no input
+    @LISTENINGA
+    0; JMP  // Keep Listening
+
+// 256 * 512
+// for i = 0; i < 256*512/16; i++
+// set SCREEN[addr + i] =  action_bit
+//
+(SET_LOOP)
+    @i
+    M=0   // initialized i to 0 
+
+(INNER_LOOP)
+    @i
+    D=M 
+    @8192
+    D=D-A
+    @LISTENING
+    D; JEQ    // if i == 8192; set complete, back to listening,
+
+    @SCREEN
+    D=A
+    @i
+    D=D+M 
+    @curr_addr
+    M=D
+
+    @action_bit
+    D=M
+    @curr_addr
+    A=M
+    M=D     // set curr_addr screen to action_bit
+    @i
+    M=M+1   //increment i
+    @INNER_LOOP
+    0; JMP  // Keep looping
+
+(SET)
+    @set
+    D=M
+    @LISTENING
+    D; JNE    // Keep Listening if already set
+
+    @set
+    M=1
+    @action_bit
+    M=-1
+    @SET_LOOP
+    0; JMP    // set screen
+
+(CLEAR)
+    @set
+    D=M
+    @LISTENING
+    D; JEQ    // Keep Listening if already cleared
+
+    @set
+    M=0
+    @action_bit
+    M=0
+    @SET_LOOP
+    0; JMP    // clear screen
--- ComputerScience/FromNandtoTetris2/nand2tetris/projects/04/mult/mult.asm
+++ ComputerScience/FromNandtoTetris2/nand2tetris/projects/04/mult/mult.asm
@@ -7,3 +7,60 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// Pseudo Code
+// if R1 < 0;
+// R1 = - R1, R0 = -R0
+// 
+// sum = 0
+// for i = 0; i < R1; i++
+//    sum = sum + R0
+// R2 = sum
+// End Pseudo Code
+
+// Multiple RAM[1] and RAM[0]
+    @R1
+    D=M
+    @CHANGE_SIGN
+    D; JLT // Jump to CHANGE_SIGN if R1 < 0 
+
+    @i
+    M=0
+
+    @R2
+    M=0
+
+(LOOP)
+    @i
+    D=M
+    @R1
+    D=D-M; 
+    @END
+    D; JEQ // End Loop if i - RAM[1]  == 0
+
+    @R2
+    D=M
+    @R0
+    D=D+M
+    @R2
+    M=D
+
+    @i
+    M = M+1
+    @LOOP
+    0; JMP 
+
+(CHANGE_SIGN)
+    @R1
+    D=-M
+    M=D
+
+    @R0
+    D=-M
+    M=D
+    @LOOP
+    0; JMP
+
+(END)
+    @END
+    0; JMP 
